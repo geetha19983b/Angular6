41faeff020753c328d18743d86855ff3984ad3f7
Geetha <geethainfy@gmail.com>
2020-01-06 00:12:38 +0530

    initial

---
+++ src/app/employee/create-employee.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import {FormGroup,FormControl,FormBuilder,Validators} from '@angular/forms';
+@Component({
+  selector: 'app-create-employee',
+  templateUrl: './create-employee.component.html',
+  styleUrls: ['./create-employee.component.css']
+})
+export class CreateEmployeeComponent implements OnInit {
+  employeeForm:FormGroup;
+  constructor(private fb:FormBuilder) { }
+
+  ngOnInit() {
+    /* this.employeeForm=new FormGroup({
+      fullName:new FormControl(),
+      email:new FormControl(),
+      skills:new FormGroup({
+        skillName:new FormControl(),
+        experienceInYears:new FormControl(),
+        proficiency:new FormControl()
+      })
+    }); */
+    this.employeeForm=this.fb.group({
+      fullName:['',[Validators.required,Validators.minLength(2),
+      Validators.maxLength(10)]],
+      email:[''],
+      skills:this.fb.group({
+        skillName:[''],
+        experienceInYears:[''],
+        proficiency:['beginner']
+      })
+    });
+
+  }
+
+  onLoadDataClick():void{
+    this.employeeForm.patchValue({
+      fullName:'Rithvik Tech',
+     email:'ranga@gmail.com'
+    });
+    /* this.employeeForm.setValue({
+     fullName:'Rithvik Tech',
+     email:'ranga@gmail.com',
+     skills:{
+       skillName:'C#',
+       experienceInYears:5,
+       proficiency:'beginner'
+     }
+    }); */
+  }
+  onSubmit():void{
+    console.log(this.employeeForm.value);
+  }
+
+}
